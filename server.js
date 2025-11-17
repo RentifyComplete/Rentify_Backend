ae08930b29462f635288548814f0244d8cae78ca
Kartikshr <147167458+Kartikshr@users.noreply.github.com>
2025-11-17 18:16:42 +0530

    Add payment routes to server

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const paymentRoutes = require('./routes/payment');
+app.use('/api/payments', paymentRoutes);
+
 // Middleware
 app.use(cors());
 app.use(express.json());
